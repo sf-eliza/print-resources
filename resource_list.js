47c1f11daff9c79125bdf01e9108148f9b8e15c7
sf-eliza <eliza@schoolfrontiers.com>
2025-10-23 14:45:59 -0400

    just resources

---
+++ resource_list.js
@@ -0,0 +1,80 @@
+const resources = [
+    [
+      "5 Ways to Ensure You Have an Effective MTSS System",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/2b479d68-b92f-416b-8e7a-6d2c2e9cce68/Screenshot+2025-09-02+141036.png?format=750w",
+      "/s/Five-Ways-to-Ensure-You-have-an-Effective-MTSS-System.pdf",
+      ["School Performance & MTSS Coaching"]
+    ],
+    [
+      "5 Things to Know about Strategic Planning",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/53cdd1d2-8635-4934-a404-91a00297a64d/Screenshot+2025-09-02+142416.png?format=750w",
+      "/s/5-Things-to-Know-about-Strategic-Planning.pdf",
+      ["Strategic Planning & Impact Evaluation"]
+    ],
+    [
+      "Appropriate State Certification Requirements for ESSA Funded Employees",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/dca31759-5642-40f2-a900-a3bdd9aca774/Screenshot+2025-09-02+142643.png?format=750w",
+      "/s/ASC-Guide-for-ESSA.pdf",
+      ["Federal Programs & State Reporting"]
+    ],
+    [
+      "Federal Programs Calendars",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/a64d3e66-db7b-4bbb-9c64-b42e2c6b0aa1/Screenshot+2025-09-02+142808.png?format=750w",
+      "/s/Federal-Programs-Calendars.pdf",
+      ["Federal Programs & State Reporting"]
+    ],
+    [
+      "Make a Strategic Plan Result in Action",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/6212f60a-f6b1-4203-af1e-2f2f3e70598e/Screenshot+2025-09-02+142835.png?format=750w",
+      "/s/Make-a-Strategic-Plan-Result-in-Action.pdf",
+      ["Strategic Planning & Impact Evaluation"]
+    ],
+    [
+      "PFE ESSA Requirements Checklist",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/a45d2756-a786-49d9-8eff-ba05ee7e3585/Screenshot+2025-09-02+142857.png?format=750w",
+      "/s/PFE-ESSA-Requirements-Checklist.pdf",
+      ["Federal Programs & State Reporting"]
+    ],
+    [
+      "Sample Federal Programs Coordinator Board Report",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/a6325456-d9de-45a3-acf6-a7b27de86e2f/Screenshot+2025-09-02+142918.png?format=750w",
+      "/s/Sample-Federal-Programs-Coordinator-Board-Report-ylel.pdf",
+      ["Federal Programs & State Reporting"]
+    ],
+    [
+      "School Performance & MTSS Coaching Overview",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/15e2ddc8-cd86-4ded-bbe0-bc8c7539ce40/Screenshot+2025-09-02+142939.png?format=750w",
+      "/s/School-Performance-MTSS-Coaching-jfft.pdf",
+      ["School Performance & MTSS Coaching"]
+    ],
+    [
+      "School Accelerator Case Study",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/fe7b5a95-eb6f-4df5-b864-61385eb0a137/Screenshot+2025-09-02+142949.png?format=750w",
+      "/s/School-Accelerator-white-paper-School-Frontiers-2025.pdf",
+      ["School Performance & MTSS Coaching", "Case Study"]
+    ],
+    [
+      "AEA Presentation",
+      " https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/1a8246e8-02f4-49fe-89d4-365e1205fa2d/Screenshot+2025-09-15+at+12.08.59%E2%80%AFPM.png",
+      "/s/AEA-Presentation-v2a2.pdf",
+      ["Strategic Planning & Impact Evaluation","Presentations"]
+    ],
+    [
+      "NM Charter Presentation Dec 2024",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/42d8239e-9664-4ef0-8bbc-a56182626108/Screenshot+2025-09-16+153416.png",
+      "/s/School-Frontiers-NM-CS-Conference-Presentation-December-2024-ys5g.pdf",
+      ["School Performance & MTSS Coaching","Presentations"]
+    ],
+    [
+      "PCPCS CEO Summit May 2025",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/4782bfe1-752e-4f45-99bb-722c6d6c2c63/Screenshot+2025-09-16+153346.png",
+      "/s/PCPCS-CEO-Summit-May-2025-1.pdf",
+      ["Presentations"]
+    ],
+	[
+      "Using Data For Lasting Change",
+      "https://images.squarespace-cdn.com/content/v1/6890c5c76857fe20a5bed7a4/d5f82f92-0a04-4fb3-9f2f-e142b7daea74/Screenshot+2025-10-23+140406.png",
+      "/s/Data-Driven-Strategic-Planning-for-Schools-FINAL-2.pdf",
+      ["Presentations", "Strategic Planning & Impact Evaluation"]
+    ],
+  ];
